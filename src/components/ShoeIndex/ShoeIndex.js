dc60c601a6ec9e89130ec8f7939505fd4b318c35
Bharat Pareek <bharat.pareek1991@gmail.com>
2022-11-29 19:02:40 +0530

    Fix breadcrumb display for <= tablet.

--- src/components/ShoeIndex/ShoeIndex.js
+++ src/components/ShoeIndex/ShoeIndex.js
@@ -9,12 +9,26 @@ import Spacer from '../Spacer';
 import ShoeSidebar from '../ShoeSidebar';
 import ShoeGrid from '../ShoeGrid';
 
+const ShoeIndexBreadcrumbs = () => {
+  return (
+    <Breadcrumbs>
+      <Breadcrumbs.Crumb href="/">Home</Breadcrumbs.Crumb>
+      <Breadcrumbs.Crumb href="/sale">Sale</Breadcrumbs.Crumb>
+      <Breadcrumbs.Crumb href="/sale/shoes">Shoes</Breadcrumbs.Crumb>
+    </Breadcrumbs>
+  );
+};
+
 const ShoeIndex = ({ sortId, setSortId }) => {
   return (
     <Wrapper>
       <MainColumn>
         <Header>
+          <MobileBreadcrumb>
+            <ShoeIndexBreadcrumbs/>
+          </MobileBreadcrumb>
           <Title>Running</Title>
+          <SelectWrapper>
           <Select
             label="Sort"
             value={sortId}
@@ -23,18 +37,15 @@ const ShoeIndex = ({ sortId, setSortId }) => {
             <option value="newest">Newest Releases</option>
             <option value="price">Price</option>
           </Select>
+          </SelectWrapper>
         </Header>
         <Spacer size={32} />
         <ShoeGrid />
       </MainColumn>
       <LeftColumn>
-        <Breadcrumbs>
-          <Breadcrumbs.Crumb href="/">Home</Breadcrumbs.Crumb>
-          <Breadcrumbs.Crumb href="/sale">Sale</Breadcrumbs.Crumb>
-          <Breadcrumbs.Crumb href="/sale/shoes">
-            Shoes
-          </Breadcrumbs.Crumb>
-        </Breadcrumbs>
+          <DesktopBreadcrumb>
+            <ShoeIndexBreadcrumbs/>
+          </DesktopBreadcrumb>
         <Spacer size={42} />
         <ShoeSidebar />
       </LeftColumn>
@@ -65,11 +76,31 @@ const Header = styled.header`
   display: flex;
   justify-content: space-between;
   align-items: baseline;
+  flex-wrap: wrap;
 `;
 
 const Title = styled.h2`
   font-size: 1.5rem;
   font-weight: ${WEIGHTS.medium};
+`;
+
+const DesktopBreadcrumb = styled.div`
+  @media ${MEDIAQUERIES.tabletAndSmaller} {
+    display: none;
+  }
+`;
+const MobileBreadcrumb = styled.div`
+  display: none;
+  @media ${MEDIAQUERIES.tabletAndSmaller} {
+    display: revert;
+    flex: 1 1 100%;
+  }
+`;
+
+const SelectWrapper = styled.div`
+  @media ${MEDIAQUERIES.phoneAndSmaller} {
+    display: none;
+  }
 `;
 
 export default ShoeIndex;
